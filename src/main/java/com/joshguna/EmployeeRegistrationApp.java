48a513aca3ab76d72b0708af46759a8688d529e7
Joshgun <joshgunf@gmail.com>
2023-03-15 16:16:35 -0400

    Initial commit, created project and added dependencies

---
+++ src/main/java/com/joshguna/EmployeeRegistrationApp.java
@@ -0,0 +1,13 @@
+package com.joshguna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRegistrationApp.class, args);
+    }
+
+}
