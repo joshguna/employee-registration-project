f30048bb89302dfcb2c1e2971a27f48257c5324b
Joshgun <joshgunf@gmail.com>
2023-03-15 18:43:01 -0400

    Updated HTML file. Added new form elements and Data Generator

---
+++ src/main/java/com/joshguna/bootstrap/DataGenerator.java
@@ -0,0 +1,73 @@
+package com.joshguna.bootstrap;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<String> getAllStates() {
+
+        return Arrays.asList(
+                "AK",
+                "AL",
+                "AR",
+                "AS",
+                "AZ",
+                "CA",
+                "CO",
+                "CT",
+                "District of Columbia",
+                "DE",
+                "FL",
+                "GA",
+                "GU",
+                "HI",
+                "IA",
+                "ID",
+                "IL",
+                "IN",
+                "KS",
+                "KY",
+                "LA",
+                "MA",
+                "MD",
+                "ME",
+                "MI",
+                "MN",
+                "MO",
+                "MP",
+                "MS",
+                "MT",
+                "NC",
+                "ND",
+                "NE",
+                "NH",
+                "NJ",
+                "NM",
+                "NV",
+                "NY",
+                "OH",
+                "OK",
+                "OR",
+                "PA",
+                "PR",
+                "RI",
+                "SC",
+                "SD",
+                "TN",
+                "TX",
+                "UM",
+                "UT",
+                "VA",
+                "VI",
+                "VT",
+                "WA",
+                "WI",
+                "WV",
+                "WY"
+        );
+
+
+    }
+
+}
